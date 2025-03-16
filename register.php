d483489e0d20883b97cfd5790e9f1c3a6216abec
hari kiran <harikiranventrapragada@gmail.com>
2025-03-16 16:39:29 +0530

    fix: registration form cahnged(added phone number)

--- register.php
+++ register.php
@@ -1,32 +1,38 @@
 <?php 
 
 include 'connect.php';
-
 if(isset($_POST['signUp'])){
-    $firstName=$_POST['fName'];
-    $lastName=$_POST['lName'];
-    $email=$_POST['email'];
-    $password=$_POST['password'];
-    $password=md5($password);
+    $firstName = mysqli_real_escape_string($conn, $_POST['fName']);
+    $lastName = mysqli_real_escape_string($conn, $_POST['lName']);
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $password = mysqli_real_escape_string($conn, $_POST['password']);
+    $address = mysqli_real_escape_string($conn, $_POST['address']);
+    $phone = mysqli_real_escape_string($conn, $_POST['phone']);
 
-     $checkEmail="SELECT * From users where email='$email'";
-     $result=$conn->query($checkEmail);
-     if($result->num_rows>0){
-        echo "Email Address Already Exists !";
-     }
-     else{
-        $insertQuery="INSERT INTO users(firstName,lastName,email,password)
-                       VALUES ('$firstName','$lastName','$email','$password')";
-            if($conn->query($insertQuery)==TRUE){
-                header("location: index.php");
-            }
-            else{
-                echo "Error:".$conn->error;
-            }
-     }
-   
+    // Encrypt password
+    $password = md5($password);
 
+    // Check if email already exists
+    $checkEmail = "SELECT * FROM users WHERE email='$email'";
+    $result = $conn->query($checkEmail);
+
+    if($result->num_rows > 0){
+        echo "Email Address Already Exists!";
+    } else {
+        // Insert new user with Address & Phone
+        $insertQuery = "INSERT INTO users (firstName, lastName, email, password, address, phone)
+                        VALUES ('$firstName', '$lastName', '$email', '$password', '$address', '$phone')";
+
+        if($conn->query($insertQuery) === TRUE){
+            header("Location: index.php");
+            exit();
+        } else {
+            echo "Error: " . $conn->error;
+        }
+    }
 }
+
+
 
 if(isset($_POST['signIn'])){
    $email = $_POST['email'];
